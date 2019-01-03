f208b87df32e9b3b584a0ff16f55a12997fd69ef
dpisonero <davidpisonero96@gmail.com>
2019-01-03 12:47:27 +0100

    Ready

--- AppReactRedux/Components/GameScreen.js
+++ AppReactRedux/Components/GameScreen.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import '../App.css';
 import { connect } from 'react-redux';
 import Game from "./Game";
 import { questionAnswer } from "../redux/actions";
